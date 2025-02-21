00212938c10d871b8cce153964c66a0d6956b20a
SantiSaaUCB <santiago.saavedra@ucb.edu.bo>
2025-02-21 15:00:02 -0400

    feat: Retorna I, II, III

--- src/romanos.js
+++ src/romanos.js
@@ -2,6 +2,12 @@ function romanos(num) {
   if(num == 1){
     return "I"
   }
+  else if(num == 2){
+    return "II"
+  }
+  else if(num == 3){
+    return "III"
+  }
 }
 
 export default romanos;
--- src/romanos.test.js
+++ src/romanos.test.js
@@ -4,4 +4,10 @@ describe("Romanos", () => {
   it("deberia retornar 'I' si el numero es 1", () => {
     expect(romanos(1)).toEqual("I");
   });
+  it("deberia retornar 'II' si el numero es 2", () => {
+    expect(romanos(2)).toEqual("II");
+  });
+  it("deberia retornar 'III' si el numero es 3", () => {
+    expect(romanos(3)).toEqual("III");
+  });
 });
